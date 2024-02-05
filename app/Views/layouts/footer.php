38a7a6d8542eb49aad2282e28582eae4ada9536b
Wilson Vicente <wilsonvicentelucas@gmail.com>
2024-02-05 03:37:35 +0100

    P-4

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -22,8 +22,7 @@
             </div>
         </footer>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </html>
